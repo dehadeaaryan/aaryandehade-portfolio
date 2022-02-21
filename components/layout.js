6625ac5b6e76bb4be2a8419abf2a13a7ba7f7d9f
dehadeaaryan <dehadeaaryan@gmail.com>
2022-02-20 21:01:58 -0600

    Changed head tag, added robots, added sitemap

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,11 @@
 import Navbar from './navbar'
 import Footer from './footer'
-import Head from 'next/head'
 
 import styles from '../styles/layout.module.css'
 
 export default function Layout({ children }) {
     return (
         <div className={styles.container}>
-            <Head>
-                <title>Aaryan Dehade</title>
-                <meta name="description" content="Aaryan's online portfolio" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
             <Navbar className={styles.navbar}/>
             <main className={styles.main}>{children}</main>
             <Footer className={styles.footer}/>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import Layout from '../components/layout'
 
 export default function Aaryandehade({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Aaryan Dehade</title>
+
+        <meta name="description" content="Aaryan's online portfolio" />
+        <meta charset="utf-8" />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
