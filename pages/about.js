183d5ebddc6b96ee17ecf278721e3c8ac22eb966
dehadeaaryan <dehadeaaryan@gmail.com>
2022-03-02 14:10:57 -0600

    fix

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,8 @@ import styles from '../styles/index.module.css'
 
 const tcu = <a href='https://tcu.edu' target="_blank" rel="noopener noreferrer">Texas Christian University.</a>
 
-const about = <p>
-Aaryan Dehade is currently pursuing BS in Computer Science at {tcu}
-He is a freshman with a passion for programming and learning new technologies.
+const about = <p className={styles.about}>
+Aaryan Dehade is currently pursuing BS in Computer Science at {tcu} He is a freshman with a passion for programming and learning new technologies.
 </p>
 
 export default function Index() {
