07da99a89cb8c6ae3483647f4aeeaa1df37ef535
dehadeaaryan <dehadeaaryan@gmail.com>
2023-01-26 12:51:43 -0600

    JANUARY UPDATE

--- pages/new/resume.js
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/new/index.module.css'
-
-export default function Resume() {
-    return (
-        <div>
-            <embed src='../../public/resume.pdf' className={styles.embed} />
-        </div>
-    )
-}
