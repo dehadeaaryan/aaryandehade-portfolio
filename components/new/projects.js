6d7d188490489b29a03bfe2b82e6c6d308d84f44
dehadeaaryan <dehadeaaryan@gmail.com>
2023-04-08 01:42:29 -0500

    Projects Updated

--- components/new/projects.js
+++ components/new/projects.js
@@ -6,14 +6,16 @@ export default function ProjectsOld() {
     return (
         <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} transitionSpeed={2000} gyroscope={true} className={styles.tiltingArea}>
         <div className={styles.container}>
-            <Project title="Turtler" link="https://github.com/dehadeaaryan/turtler" />
-            <Project title="discord-bot-maker" link="https://github.com/dehadeaaryan/discord-bot-maker" />
-            <Project title="pygame-builder" link="https://github.com/dehadeaaryan/pygame-builder" />
+            <Project title="Marie Simulator" link="https://github.com/dehadeaaryan/MarieSimulator" />
             <Project title="JustDropIt" link="https://github.com/justDropIt/JustDropIt" />
-            <Project title="TCU Utils" link="https://github.com/dehadeaaryan/tcuutilities" />
+            <Project title="discord-bot-maker" link="https://github.com/dehadeaaryan/discord-bot-maker" />
             <Project title="Music.www" link="https://github.com/dehadeaaryan/music.www---new" />
             <Project title="Quacker" link="https://github.com/dehadeaaryan/quacker-bot-master" />
+            <Project title="Turtler" link="https://github.com/dehadeaaryan/turtler" />
+            <Project title="pygame-builder" link="https://github.com/dehadeaaryan/pygame-builder" />
+            <Project title="Chess game" link="https://github.com/dehadeaaryan/chess" />
             <Project title="Sudoku Solver" link="https://github.com/dehadeaaryan/Sudoku-Solver" />
+            <Project title="TCU Utils" link="https://github.com/dehadeaaryan/tcuutilities" />
             <Project title="More" link="https://github.com/dehadeaaryan?tab=repositories" />
             
         </div>
