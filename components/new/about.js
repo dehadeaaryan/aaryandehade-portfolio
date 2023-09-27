4cb6961a62d967c82856653233337355916a3fa4
dehadeaaryan <dehadeaaryan@gmail.com>
2023-09-27 18:36:53 -0500

    trying to correct error for apostrophe

--- components/new/about.js
+++ components/new/about.js
@@ -3,7 +3,7 @@ import styles from '../../styles/new/about.module.css'
 
 const tcu = <a href='https://tcu.edu' target="_blank" rel="noopener noreferrer">Texas Christian University</a>
 const about = <p className={styles.about}>
-Hi! I'm Aaryan Dehade, a Computer Science enthusiast currently pursuing my Bachelor of Science degree at {tcu}, with a minor in Mathematics. Feel free to explore my experiences and connect with me on this platform.
+Hi! I am Aaryan Dehade, a Computer Science enthusiast currently pursuing my Bachelor of Science degree at {tcu}, with a minor in Mathematics. Feel free to explore my experiences and connect with me on this platform.
 </p>
 
 export default function About() {
