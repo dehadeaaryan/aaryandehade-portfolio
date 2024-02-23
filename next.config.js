e74ee6d48f993ad948365abfc1f3e2eb66674f42
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-23 10:47:17 -0600

    add lang

--- next.config.js
+++ next.config.js
@@ -1,48 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/new/',
-        permanent: true,
-      },
-    ]
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/new/',
+                permanent: true,
+            },
+        ]
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        })
 
-    return config
-  },
+        return config
+    },
 
-  async rewrites() {
-    return [
-        {
-          source: '/new',
-          destination: '/blog',
-        },
-        {
-            source: '/blog',
-            destination: '/new',
-        },
-      ]
-    var obj = {
-        source: '/:path*',
-        has: [
-              {
-                type: 'host',
-                value: 'blog.*',
-              },
-        ],
-        destination: `/blog`,
-      }
-    return [obj]
-  },
+    async rewrites() {
+        return [
+            {
+                source: '/new',
+                destination: '/blog',
+            },
+            {
+                source: '/blog',
+                destination: '/new',
+            },
+        ]
+        var obj = {
+            source: '/:path*',
+            has: [
+                {
+                    type: 'host',
+                    value: 'blog.*',
+                },
+            ],
+            destination: `/blog`,
+        }
+        return [obj]
+    },
 }
 
 module.exports = nextConfig
