97c2ebdd220dd4f7c95fdf008e4553f6763bb801
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-23 10:27:09 -0600

    google verification

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ export default function Aaryandehade({ Component, pageProps }) {
         <title>Aaryan Dehade</title>
 
         <meta name="description" content="Aaryan Dehade" />
+        <meta name="google-site-verification" content="O_S6fiFOgdsf5ziwjChZ7KyC5trKDE5woPZuOUwRDOA" />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pixelify+Sans" />
