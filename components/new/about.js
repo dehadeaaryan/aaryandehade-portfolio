3a53b30e7e0fab4957278795618556f1f556145f
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-24 19:32:15 -0600

    fixed apos error

--- components/new/about.js
+++ components/new/about.js
@@ -3,7 +3,7 @@ import styles from '../../styles/new/about.module.css'
 
 const tcu = <a href='https://tcu.edu' target="_blank" rel="noopener noreferrer">Texas Christian University</a>
 const about = <p className={styles.about}>
-Greetings! I'm Aaryan Dehade, a dedicated Computer Science student at {tcu}, also specializing in Mathematics. Explore my journey, projects, and accomplishments on this platform. Let's connect and share our experiences in the dynamic world of technology!
+Greetings! I&apos;m Aaryan Dehade, a dedicated Computer Science student at {tcu}, also specializing in Mathematics. Explore my journey, projects, and accomplishments on this platform. Let&apos;s connect and share our experiences in the dynamic world of technology!
 </p>
 
 export default function About() {
