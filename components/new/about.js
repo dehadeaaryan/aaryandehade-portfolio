753ccdc183dd31cfa7d49af7c3414ca8745fd731
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-24 19:29:08 -0600

    Changed about me

--- components/new/about.js
+++ components/new/about.js
@@ -3,7 +3,7 @@ import styles from '../../styles/new/about.module.css'
 
 const tcu = <a href='https://tcu.edu' target="_blank" rel="noopener noreferrer">Texas Christian University</a>
 const about = <p className={styles.about}>
-Hi! I am Aaryan Dehade, a Computer Science enthusiast currently pursuing my Bachelor of Science degree at {tcu}, with a minor in Mathematics. Feel free to explore my experiences and connect with me on this platform.
+Greetings! I'm Aaryan Dehade, a dedicated Computer Science student at {tcu}, also specializing in Mathematics. Explore my journey, projects, and accomplishments on this platform. Let's connect and share our experiences in the dynamic world of technology!
 </p>
 
 export default function About() {
